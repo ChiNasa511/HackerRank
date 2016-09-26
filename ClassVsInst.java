8f2329428d8148f7e66443d19cc706e792b5edbe
Chinasa T. Okolo <theonasa511@hotmail.com>
2016-09-26 02:17:31 -0700

    Create ClassVsInst.java

---
+++ ClassVsInst.java
@@ -0,0 +1,35 @@
+// Day 4
+//
+// @author Chinasa T. Okolo 
+
+public class Person {
+    private int age;	
+    String statement;
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+        if (initialAge >= 0) {
+            age = initialAge;
+        } else {
+            age = 0; 
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+    }
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if (age < 13) {
+            statement = "young";
+        } else if (age >= 13 && age < 18) {
+            statement = "a teenager"; 
+        } else {
+            statement = "old"; 
+        }
+        System.out.println("You are " + statement + ".");
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+        age++;
+	}
+    
